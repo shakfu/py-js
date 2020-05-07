ec54e74e09146cb44635e8fd37c0433081f6d950
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-05-07 06:36:27 +0300

    preliminary build success after codesign issues

--- README.md
+++ README.md
@@ -3,3 +3,46 @@
 An attempt to make a max external for python
 
 
+## Building
+
+place the following script in source folder of max-sdk-8.0.3
+
+```ruby
+#!/usr/bin/env ruby -wKU
+# encoding: utf-8
+
+puts "cleanup"
+system "rm -rf ../externals/py.mxo && rm -rf basics/py/build && xattr -cr ./basics/py"
+
+sdk_examples_dir = "."
+Dir.chdir sdk_examples_dir
+sdk_examples_dir = Dir.pwd
+
+def build_projects_for_dir(path)
+  puts
+  puts "Building Projects for Directory: #{path}"
+  
+  Dir.foreach path do |filename|
+
+    puts "scanning: #{filename}"
+
+    if filename.match(/.*\.xcodeproj/)
+      puts "  Building #{filename}" 
+      result = `cd "#{path}"; xcodebuild -project #{filename} 2>&1`
+      puts result
+      if result.match(/\*\* BUILD SUCCEEDED \*\*/)
+        puts "    (success)"
+      else
+        puts "    (FAIL) ************************************"
+        #puts result
+      end
+    end
+
+  end
+  
+end
+
+build_projects_for_dir("basics/py")
+
+puts "You now have a Max Package that you can use."
+```
---
+++ build/py.build/Development/max-external.build/Objects-normal/x86_64/py.d
@@ -0,0 +1,106 @@
+dependencies: \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/basics/py/py.c \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/basics/py/../../c74support/max-includes/macho-prefix.pch \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_common.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/max_types.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_prefix.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_mess.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_maxtypes.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_byteorder.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_sysmem.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_sysfile.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_systime.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_expr.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_path.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_wind.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_proto.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_atomarray.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_atombuf.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_obstring.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_hashtab.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_linklist.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_atomic.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_dictionary.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_obex_util.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/commonsyms.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_strings.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_obex.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_preprocessor.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_systhread.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_drag.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/jpatcher_api.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/jpatcher_syms.h \
+  /Users/sa/Documents/Max\ 8/Packages/max-sdk-8.0.3/source/c74support/max-includes/ext_charset.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/Python.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/patchlevel.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyconfig.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pymacconfig.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyport.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pymacro.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyatomic.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pymath.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pytime.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/object.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pymem.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/objimpl.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/typeslots.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyhash.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pydebug.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/bytearrayobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/bytesobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/unicodeobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/longobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/longintrepr.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/boolobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/floatobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/complexobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/rangeobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/memoryobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/tupleobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/listobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/dictobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/odictobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/enumobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/setobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/methodobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/moduleobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/funcobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/classobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/fileobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pycapsule.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/traceback.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pystate.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pythread.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/sliceobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/cellobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/iterobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/genobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/descrobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/warnings.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/weakrefobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/structseq.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/namespaceobject.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/codecs.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyerrors.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/context.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyarena.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/modsupport.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/compile.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/code.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pythonrun.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pylifecycle.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/ceval.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/sysmodule.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/osmodule.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/intrcheck.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/import.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/abstract.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/bltinmodule.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/eval.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyctype.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pystrtod.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pystrcmp.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/dtoa.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/fileutils.h \
+  /usr/local/Cellar/python/3.7.7/Frameworks/Python.framework/Versions/3.7/include/python3.7m/pyfpe.h
