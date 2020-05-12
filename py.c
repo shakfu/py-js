617ba439d8caca3af3243b703e128cfb29ca815a
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-05-12 10:41:56 +0300

    cleanup

--- py.c
+++ py.c
@@ -47,8 +47,6 @@ typedef struct _py {
     t_object p_ob;          // object header - ALL objects MUST begin with this...
     t_symbol *p_module;     // python import: additional imports
     t_symbol *p_code;       // python code to evaluate to default outlet
-    long p_value0;          // int value - received from the left inlet and stored internally for each object instance
-    long p_value1;          // int value - received from the right inlet and stored internally for each object instance
     void *p_outlet;         // outlet creation - inlets are automatic, but objects must "own" their own outlets
 } t_py;
 
@@ -58,10 +56,7 @@ void py_bang(t_py *x);
 void py_import(t_py *x, t_symbol *s);
 void py_eval(t_py *x, t_symbol *s, long argc, t_atom *argv);
 void py_run(t_py *x, t_symbol *s, long argc, t_atom *argv);
-void py_int(t_py *x, long n);
-void py_in1(t_py *x, long n);
 void py_dblclick(t_py *x);
-void py_assist(t_py *x, void *b, long m, long a, char *s);
 
 void *py_new(t_symbol *s, long argc, t_atom *argv);
 void py_free(t_py *x);
@@ -82,13 +77,10 @@ void ext_main(void *r)
     class_addmethod(c, (method)py_bang,    "bang",      0);
     class_addmethod(c, (method)py_import,  "import",    A_DEFSYM, 0);
     class_addmethod(c, (method)py_eval,    "anything",  A_GIMME, 0);
-    class_addmethod(c, (method)py_eval,    "run",       A_GIMME, 0);
-    class_addmethod(c, (method)py_int,     "int",       A_LONG, 0);
-    class_addmethod(c, (method)py_in1,     "in1",       A_LONG, 0);
+    class_addmethod(c, (method)py_run,     "run",       A_GIMME, 0);
 
     /* you CAN'T call this from the patcher */
     class_addmethod(c, (method)py_dblclick, "dblclick", A_CANT, 0);
-    class_addmethod(c, (method)py_assist,   "assist",   A_CANT, 0);
 
     // attributes
     CLASS_ATTR_SYM(c, "module", 0, t_py, p_module);
@@ -116,18 +108,15 @@ void *py_new(t_symbol *s, long argc, t_atom *argv)
     if (x) {
         x->p_module = gensym("");
         x->p_code = gensym("");
-        x->p_value0 = 0;
-        x->p_value1 = 0;
 
         // create inlet(s)
-        intin(x,1);      // create a second int inlet (leftmost inlet is automatic - all objects have one inlet by default)
         // create outlet
         x->p_outlet = outlet_new(x, NULL);
         
         // process @arg attributes
         attr_args_process(x, argc, argv);
     }    
-    //post("new py object instance added to patch...", 0);
+    post("new py object instance added to patch...", 0);
     return(x);
 }
 
@@ -139,23 +128,6 @@ void py_free(t_py *x)
 
 
 //--------------------------------------------------------------------------
-
-void py_assist(t_py *x, void *b, long m, long a, char *s) // 4 final arguments are always the same for the assistance method
-{
-    if (m == ASSIST_OUTLET)
-        sprintf(s,"Sum of Left and Right Inlets");
-    else {
-        switch (a) {
-        case 0:
-            sprintf(s,"Inlet %ld: Left Operand (Causes Output)", a);
-            break;
-        case 1:
-            sprintf(s,"Inlet %ld: Right Operand (Added to Left)", a);
-            break;
-        }
-    }
-}
-
 
 void py_dblclick(t_py *x)
 {
@@ -289,45 +261,6 @@ void py_eval(t_py *x, t_symbol *s, long argc, t_atom *argv) {
 
 void py_bang(t_py *x)
 {
-    long sum;
-    PyObject *locals, *globals;
-    PyObject *pval, *xval, *yval;
-
-    // python init and setup
-    Py_Initialize();
-    locals = PyDict_New();
-    globals = PyDict_New();
-    PyDict_SetItemString(globals, "__builtins__", PyEval_GetBuiltins());
-
-    xval = PyLong_FromLong(x->p_value0);
-    yval = PyLong_FromLong(x->p_value1);
-
-    PyDict_SetItemString(globals, "x", xval);
-    PyDict_SetItemString(globals, "y", yval);
-
-    pval = PyRun_String("x**y", Py_eval_input, globals, locals);
-
-    if PyLong_Check(pval) {
-        sum = PyLong_AsLong(pval);
-    }
-
-    outlet_int(x->p_outlet, sum);
-
-    Py_DECREF(pval);
-    Py_DECREF(xval);
-    Py_DECREF(yval);
-    Py_FinalizeEx();
-}
-
-void py_int(t_py *x, long n)
-{
-    x->p_value0 = n;
-    py_bang(x);
-}
-
-
-void py_in1(t_py *x, long n)
-{
-    x->p_value1 = n;
+    post("bang");
 }
 
