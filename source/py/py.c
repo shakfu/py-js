624efdac454289f3295964beec7f97859e57e0d1
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-05-29 03:10:46 +0300

    snapshot of anything method

--- source/py/py.c
+++ source/py/py.c
@@ -663,7 +663,7 @@ void py_assign(t_py* x, t_symbol* s, long argc, t_atom* argv)
     }
 
     if (PyList_Size(list) != argc - 1) {
-        error("PyList_Size(list) != argc - 1");
+        py_error(x, "PyList_Size(list) != argc - 1");
         goto error;
     } else {
         py_log(x, "length of list: %d", PyList_Size(list));
@@ -684,4 +684,218 @@ error:
     Py_XDECREF(list);
 }
 
-void py_anything(t_py* x, t_symbol* s, long argc, t_atom* argv) { }
+
+void py_anything(t_py* x, t_symbol* s, long argc, t_atom* argv)
+{
+    char* py_argv = NULL;
+    PyObject* pval = NULL;
+    PyObject* py_callable_str = NULL;
+    PyObject* py_callable = NULL;
+    PyObject* py_argslist = NULL; // python list
+    PyObject* py_args = NULL; // python tuple
+    PyObject* locals = NULL;
+
+    if (s == gensym("")) {
+        py_error("could not retrieve callable name", s->s_name);
+        goto error;
+    }
+
+    py_callable_str = PyUnicode_FromString(s->s_name);
+    if (py_callable_str == NULL) {
+        py_error(x, "%s: could not convert callable name to python string");
+        goto error;
+    }
+
+    
+    if ((py_argslist = PyList_New(0)) == NULL) {
+        py_error(x, "could not create an empyt python list");
+        goto error;
+    }
+
+    for (int i = 0; i < argc; i++) {
+        switch ((argv + i)->a_type) {
+        case A_FLOAT: {
+            double c_float = atom_getfloat(argv + i);
+            PyObject* p_float = PyFloat_FromDouble(c_float);
+            if (p_float == NULL) {
+                error("p_float == NULL");
+                goto error;
+            }
+            PyList_Append(py_argslist, p_float);
+            Py_DECREF(p_float);
+            py_log(x, "%d: %f", i, atom_getfloat(argv + i));
+            break;
+        }
+        case A_LONG: {
+            PyObject* p_long = PyLong_FromLong(atom_getlong(argv + i));
+            if (p_long == NULL) {
+                py_error(x, "p_long == NULL");
+                goto error;
+            }
+            PyList_Append(py_argslist, p_long);
+            Py_DECREF(p_long);
+            py_log(x, "%d: %ld", i, atom_getlong(argv + i));
+            break;
+        }
+        case A_SYM: {
+            PyObject* p_str = PyUnicode_FromString(
+                atom_getsym(argv + i)->s_name);
+            if (p_str == NULL) {
+                py_error(x, "p_str == NULL");
+                goto error;
+            }
+            PyList_Append(py_argslist, p_str);
+            Py_DECREF(p_str);
+            py_log(x, "%d: %s", i, atom_getsym(argv + i)->s_name);
+            break;
+        }
+        default:
+            py_log(x, "cannot process unknown type");
+            break;
+        }
+    }
+
+    if (PyList_Size(py_argslist) != argc) {
+        py_error(x, "PyList_Size(list) != argc");
+        goto error;
+    } else {
+        py_log(x, "length of list: %d", PyList_Size(py_argslist));
+    }
+
+    // namespace check
+
+    if (PyDict_Contains(x->p_globals, py_callable_str) != 1) {
+        py_error(x, "callable name not found in py object namespace");
+        goto error;
+    }
+
+    py_callable = PyDict_GetItemWithError(x->p_globals, py_callable_str);
+    if (py_callable == NULL) {
+        py_error("not able to retrieve callable from object namespace");
+        goto error;
+    }
+
+    if (PyCallable_Check(py_callable) != 1) {
+        py_error(x, "given callable is actually not callable from object namespace");
+        goto error;
+    }
+
+    // convert py_args to tuple
+    py_args = PyList_AsTuple(py_argslist);
+    if (py_args == NULL) {
+        py_error(x, "unable to convert args list to tuple");
+        goto error;
+    }
+
+    pval = PyObject_Call(py_callable, py_args, NULL);
+    if (pval == NULL) {
+        py_error(x, "could not retrieve result of call");
+        goto error;
+    }
+
+
+    // handle ints and longs
+    if (PyLong_Check(pval)) {
+        long int_result = PyLong_AsLong(pval);
+        outlet_int(x->p_outlet, int_result);
+    }
+
+    // handle floats and doubles
+    if (PyFloat_Check(pval)) {
+        float float_result = (float)PyFloat_AsDouble(pval);
+        outlet_float(x->p_outlet, float_result);
+    }
+
+    // handle strings
+    if (PyUnicode_Check(pval)) {
+        const char* unicode_result = PyUnicode_AsUTF8(pval);
+        outlet_anything(x->p_outlet, gensym(unicode_result), 0, NIL);
+    }
+
+    // handle lists, tuples and sets
+    if (PyList_Check(pval) || PyTuple_Check(pval) || PyAnySet_Check(pval)) {
+        PyObject* iter = NULL;
+        PyObject* item = NULL;
+        int i = 0;
+
+        t_atom atoms_static[PY_MAX_ATOMS];
+        t_atom* atoms = NULL;
+        int is_dynamic = 0;
+
+        Py_ssize_t seq_size = PySequence_Length(pval);
+        if (seq_size <= 0) {
+            error("cannot convert python sequence with zero or less length to "
+                  "atoms");
+            goto error;
+        }
+
+        if ((iter = PyObject_GetIter(pval)) == NULL) {
+            goto error;
+        }
+
+        if (seq_size > PY_MAX_ATOMS) {
+            post("dynamically increasing size of atom array");
+            atoms = atom_dynamic_start(atoms_static, PY_MAX_ATOMS,
+                                       seq_size + 1);
+            is_dynamic = 1;
+
+        } else {
+            atoms = atoms_static;
+        }
+
+        while ((item = PyIter_Next(iter)) != NULL) {
+            if (PyLong_Check(item)) {
+                long long_item = PyLong_AsLong(item);
+                atom_setlong(atoms + i, long_item);
+                post("%d long: %ld\n", i, long_item);
+                i++;
+            }
+
+            if PyFloat_Check (item) {
+                float float_item = PyFloat_AsDouble(item);
+                atom_setfloat(atoms + i, float_item);
+                post("%d float: %f\n", i, float_item);
+                i++;
+            }
+
+            if PyUnicode_Check (item) {
+                const char* unicode_item = PyUnicode_AsUTF8(item);
+                post("%d unicode: %s\n", i, unicode_item);
+                atom_setsym(atoms + i, gensym(unicode_item));
+                i++;
+            }
+            Py_DECREF(item);
+        }
+        outlet_list(x->p_outlet, NULL, i, atoms);
+        post("end iter op: %d", i);
+
+        if (is_dynamic) {
+            post("restoring to static atom array");
+            atom_dynamic_end(atoms_static, atoms);
+        }
+    }
+
+    // success cleanup
+    Py_XDECREF(pval);
+    post("END %s: %s", s->s_name, py_argv);
+    return;
+
+error:
+    if (PyErr_Occurred()) {
+        PyObject *ptype, *pvalue, *ptraceback;
+        PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+
+        // get error message
+        const char* pStrErrorMessage = PyUnicode_AsUTF8(pvalue);
+        error("PyException('%s %s'): %s", s->s_name, py_argv,
+              pStrErrorMessage);
+        Py_XDECREF(ptype);
+        Py_XDECREF(pvalue);
+        Py_XDECREF(ptraceback);
+    }
+    // cleanup
+    Py_XDECREF(pval);
+    // Py_XDECREF(locals);
+}
+
+
