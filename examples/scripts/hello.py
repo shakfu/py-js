9e208bc4c265f004e2c39ec020b2493467a2cd44
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-06-15 02:18:32 +0300

    minor changes

--- examples/scripts/hello.py
+++ examples/scripts/hello.py
@@ -1,12 +1,25 @@
-# embedded builtin
+# hello.py
+"""
+
+This is an demo of a python script in the `py` package.
+
+
+"""
+
 import api # cythonized max c api
 
 # basic examples
 a = 10
+
 b = 1.5
+
 c = "HELLO WORLD!!!"
+
 d = [1,2,3,4]
+
 e = ['a','b', 'c']
+
 f = lambda: "hello func"
+
 g = lambda x: x+10
 
