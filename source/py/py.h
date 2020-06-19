faf83792175c3a0599d58372fcffa56a8f9c04b5
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-06-19 14:47:43 +0300

    cleanup

--- source/py/py.h
+++ source/py/py.h
@@ -91,10 +91,10 @@ void py_locate_path_from_symbol(t_py* x, t_symbol* s);
 
 /* common handlers */
 void py_handle_error(t_py* x, char* fmt, ...);
-void py_handle_float_output(t_py* x, PyObject* pval, bool free_now);
-void py_handle_long_output(t_py* x, PyObject* pval, bool free_now);
-void py_handle_string_output(t_py* x, PyObject* pval, bool free_now);
-void py_handle_list_output(t_py* x, PyObject* pval, bool free_now);
+void py_handle_float_output(t_py* x, PyObject* pval);
+void py_handle_long_output(t_py* x, PyObject* pval);
+void py_handle_string_output(t_py* x, PyObject* pval);
+void py_handle_list_output(t_py* x, PyObject* pval);
 void py_handle_output(t_py* x, PyObject* pval);
 
 /* core python methods */
