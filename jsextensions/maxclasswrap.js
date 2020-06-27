9063e9490236b907079696bdca68dc992c2daace
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-06-27 06:34:57 +0300

    more docs for pyjs

--- jsextensions/maxclasswrap.js
+++ jsextensions/maxclasswrap.js
@@ -1,12 +1,6 @@
 /*
 
-This is a just a placeholder.
-
-Wrapping an external for js usage requires that the output
-function is changed to returm arrays instead of pushing to 
-outlets.
-
-TBD later (maybe)
+Wraps the pyjs external for use by the js object in max
 
 */
 
