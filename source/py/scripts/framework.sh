97d53c9c952b4ef3de18757217f5b3e3f61940a7
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2020-07-04 22:58:59 +0300

    packages is it / apple signing woes on embedded options

--- source/py/scripts/framework.sh
+++ source/py/scripts/framework.sh
@@ -24,28 +24,37 @@ fix_python_dylib_for_pkg() {
 fix_python_dylib_for_ext() {
 	cd $PREFIX/lib
 	chmod 777 ${DYLIB}
-	install_name_tool -id @loader_path/../FWK_TARGET ${DYLIB}
-	cp -rf ${FRAMEWORKS} $PY_EXTERNAL/Contents/Frameworks
+	install_name_tool -id @loader_path/../${FWK_TARGET} ${DYLIB}
+	cp -rf ${FRAMEWORKS} $PY_EXTERNAL/Contents
 	cd $ROOT
 }
+
+fix() {
+	cp -rf ${FRAMEWORKS} $PY_EXTERNAL/Contents
+	cd $PY_EXTERNAL/Contents/${FWK_TARGET}/lib
+	chmod 777 ${DYLIB}
+	install_name_tool -id @loader_path/../${FWK_TARGET} ${DYLIB}
+	cd $ROOT
+}
+
 
 install_python_ext() {
 	install_python
 	# fix_python_dylib_for_ext
 }
 
-if [ "$1" == "pkg" ]; then
-	echo "Installing python from source as framework into 'support' folder of package"
-	install_python_pkg
+# if [ "$1" == "pkg" ]; then
+# 	echo "Installing python from source as framework into 'support' folder of package"
+# 	install_python_pkg
 
-elif [ "$1" == "ext" ]; then
-	echo "Installing python from source as framework into 'py.mxo' external"
-	install_python_ext
+# elif [ "$1" == "ext" ]; then
+# 	echo "Installing python from source as framework into 'py.mxo' external"
+# 	install_python_ext
 
-elif [ "$1" == "build-python" ]; then
-	echo "Building from python source as framework"
-	build_python_zipped
+# elif [ "$1" == "build-python" ]; then
+# 	echo "Building from python source as framework"
+# 	build_python_zipped
 
-else
-	usage
-fi
+# else
+# 	usage
+# fi
