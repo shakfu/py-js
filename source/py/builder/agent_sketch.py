f625e23268c145eb7c143335fe84c3eec660b34a
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2021-04-04 08:40:18 +0300

    sys test works

--- source/py/builder/agent_sketch.py
+++ source/py/builder/agent_sketch.py
@@ -2,9 +2,11 @@
 
 Aims to be pure python without any dependencies except the standard library.
 
-TODO:
+## TODO
 1. make .name use consistent across project and product
-
+2. should check for existance of libs (not just static libs)
+3. should not re-run unecessarily
+4. reset should be deep, clean should be shallow
 
 
 """
@@ -938,9 +940,9 @@ class HomebrewBuilder(PyJsBuilder):
 
 
 # ------------------------------------------------------------------------------------
-# Connfiguration Classes
+# Connfiguration Examples
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bzip2_product = Product(
         name="bzip2",
         version="1.0.8",
@@ -980,4 +982,56 @@ if __name__ == '__main__':
     static_python_builder = StaticPythonBuilder(
         product=python_product, depends_on=[bzip2_builder, ssl_builder, xz_builder]
     )
+    shared_python_builder = SharedPythonBuilder(
+        product=python_product, depends_on=[bzip2_builder, ssl_builder, xz_builder]
+    )
+    framework_python_builder = FrameworkPythonBuilder(
+        product=python_product, depends_on=[bzip2_builder, ssl_builder, xz_builder]
+    )
     homebrew_builder = HomebrewBuilder(product=pyjs_product)
+
+    build_all_recipe = Recipe(
+        name="build_all",
+        builders=[
+            static_python_builder,
+            shared_python_builder,
+            framework_python_builder,
+            homebrew_builder,
+        ],
+    )
+
+    self_contained_recipe = Recipe(
+        name="self_contained",
+        builders = [
+            StaticPythonBuilder(
+                product=Product(
+                    name="Python",
+                    version=PYTHON_VERSION_STRING,
+                    url_template="https://www.python.org/ftp/python/{version}/Python-{version}.tgz",
+                    libs_static=["libpython3.9.a"],
+                ),
+                depends_on= [
+                    Bzip2Builder(product=Product(
+                        name="bzip2",
+                        version="1.0.8",
+                        url_template="https://sourceware.org/pub/bzip2/{name}-{version}.tar.gz",
+                        libs_static=["libbz2.a"],
+                    )),
+                    OpensslBuilder(product=Product(
+                        name="openssl",
+                        version="1.1.1g",
+                        url_template="https://www.openssl.org/source/{name}-{version}.tar.gz",
+                        libs_static=["libssl.a", "libcrypto.a"],
+                    )),
+                    XzBuilder(product=Product(
+                        name="xz",
+                        version="5.2.5",
+                        url_template="http://tukaani.org/xz/{name}-{version}.tar.gz",
+                        libs_static=["libxz.a"],
+                    )),
+                ]
+            )
+        ]
+    )
+
+    homebrew_builder.install_homebrew_sys()
