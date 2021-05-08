dcc42d83317879a30e630b62d52348a33868a9dc
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2021-05-08 10:20:58 +0300

    updated docs

--- source/py/zmq/zmqc/build.sh
+++ source/py/zmq/zmqc/build.sh
@@ -2,13 +2,21 @@ PROJECT="zmqc"
 XCODEPROJ=${PROJECT}.xcodeproj
 HELPFILE=${PROJECT}.maxhelp
 
+echo "building the external"
 if [[ -d ${XCODEPROJ} ]]
 then
     rm -rf ${XCODEPROJ}
 fi
 xcodegen
 xcodebuild -project ${XCODEPROJ}
+
+echo "building the minimal zmq server"
+clang -o zmqc_server -lzmq zmqc_server.c
+
+echo "copying .maxhelp to proper package location"
 cp ${HELPFILE} ../../../../help
+
+echo "deploy"
 cd ../../
 make pkg
 
