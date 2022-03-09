e034e4b78dd53ec9bb5959bd13c4391455649539
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-03-10 00:43:38 +0300

    python zpy client

--- source/projects/zmq/zpy/zpy_client.py
+++ source/projects/zmq/zpy/zpy_client.py
@@ -1,24 +1,34 @@
-#
-#   Hello World client in Python
-#   Connects REQ socket to tcp://localhost:5555
-#   Sends "Hello" to server, expects "World" back
-#
+#!/usr/bin/env python3
 
 import zmq
 
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server...")
+print("Connecting to python server...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
+
+request_response = [
+
+    ('test float', 'float 10.2'),
+    ('test int', 'int 2'),
+    ('test list', 'list 10 sam 10.2 2 hello'),
+    ('test XXX', 'test <need-type>'),
+
+    ('1+1', '2'),
+    ('XXX', 'python-eval-error'),
+]
+
+
+for request, expected_response in request_response:
     print(f"Sending request {request} ...")
-    socket.send_string("Hello2")
+    socket.send_string(request)
 
     #  Get the reply.
     message = socket.recv()
-    print(f"Received reply {request} [ {message} ]")
+    message = message.decode('utf8')
+    check = message == expected_response
+    print(f"Received reply {request} -> {check} [ {message} | {expected_response} ]")
 
