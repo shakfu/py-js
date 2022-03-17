01b2c28b94a0459e5ef4bb5d93cba3c693d3ff0f
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-03-17 15:08:42 +0300

    enhance build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
-# xattr -cr .
-#make -C source/py default
 
 #cd source/py
 #python3 -m builder brew_sys
 #cd ../../
 
-cd source/py/targets/python-sys
-python3 build.py
-cd ../../../../
+python3 source/py/targets/python-sys/build.py
 
