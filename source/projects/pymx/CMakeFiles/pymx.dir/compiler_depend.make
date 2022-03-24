4f84492559f2734647d6e5866446d4b1527852e2
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-03-24 15:38:57 +0300

    cleanup

--- source/projects/pymx/CMakeFiles/pymx.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for pymx.
-# This may be replaced when dependencies are built.
--- source/projects/pymx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sa/Downloads/projects/py-js/source/projects/pymx
-# Build directory: /Users/sa/Downloads/projects/py-js/source/projects/pymx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
