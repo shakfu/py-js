3895b25f3faba3b2708ad17a82502395d55cf835
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-11-05 20:08:11 +0300

    beeware tests

--- source/projects/py/scripts/get_beeware.py
+++ source/projects/py/scripts/get_beeware.py
@@ -3,6 +3,51 @@ get tweaked / statically built components from beeware Python-Apple-support proj
 
 https://github.com/beeware/Python-Apple-support
 
+Note that the structure of beeware python releases changed after python 3.7
+
+3.7
+├── BZip2
+│   └── Headers
+├── OpenSSL
+│   └── Headers
+├── Python
+│   ├── Headers
+│   └── Resources
+└── XZ
+    └── Headers
+
+3.8-3.11
+├── Python.xcframework
+│   └── macos-arm64_x86_64
+│        └── Headers
+└── python-stdlib
+    ├── asyncio
+    ├── collections
+    ├── concurrent
+    ├── ctypes
+    ├── curses
+    ├── dbm
+    ├── distutils
+    ├── email
+    ├── encodings
+    ├── html
+    ├── http
+    ├── importlib
+    ├── json
+    ├── lib-dynload
+    ├── lib2to3
+    ├── logging
+    ├── multiprocessing
+    ├── pydoc_data
+    ├── sqlite3
+    ├── unittest
+    ├── urllib
+    ├── venv
+    ├── wsgiref
+    ├── xml
+    ├── xmlrpc
+    └── zoneinfo
+
 """
 import os
 from pathlib import Path
@@ -12,17 +57,19 @@ URL = ('https://github.com/beeware/Python-Apple-support/releases/'
 
 
 VERSIONS = {
-    '3.7' : 'b8',
-    '3.8' : 'b7',
-    '3.9' : 'b5',
-    '3.10': 'b1',
+    '3.7' : 'b9',
+    '3.8' : 'b12',
+    '3.9' : 'b10',
+    '3.10': 'b6',
+    '3.11': 'b1',
+
 }
 
 def cmd(shellcmd):
     print(shellcmd)
     os.system(shellcmd)
 
-def get_python_libs(destination_dir: str, version: str = '3.9'):
+def get_python_libs(destination_dir: str, version: str = '3.8'):
     """get tweaked / statically built components from beeware Python-Apple-support project
 
     see: https://github.com/beeware/Python-Apple-support
