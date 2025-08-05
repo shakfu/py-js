c28dc1d9ae8f496ec6ec34197b688e4c92d3b21f
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-08-05 11:32:33 +0300

    snap

---
+++ source/scripts/cmake/win_pkg_cleanup.cmake
@@ -0,0 +1,13 @@
+# delete the `libs` and `include` folder in `py-js/support`
+#
+# to be run as: cmake -P source/scripts/win_pkg_cleanup.cmake
+
+set(SUPPORT_DIR ${CMAKE_SOURCE_DIR}/support)
+set(lib_dir ${SUPPORT_DIR}/libs)
+set(include_dir ${SUPPORT_DIR}/include)
+
+cmake_path(NORMAL_PATH lib_dir)
+cmake_path(NORMAL_PATH include_dir)
+
+file(REMOVE_RECURSE ${lib_dir})
+file(REMOVE_RECURSE ${include_dir})
